93f7cffb02b8f36ad10afa5824ff59104dc19f97
Aimal Amiri <meaimal@hotmail.com>
2022-09-14 23:04:16 +0430

    Fix linter errors

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const burger = document.querySelector('#mobile-menu');
 
 burger.addEventListener('click', () => {
-  const nav = document.querySelector("#desktop-nav");
-  const links = document.querySelector("#desktop-nav ul");
-  for (const link of links.children) {
-    link.children[0].classList.toggle('text-white');
+  const nav = document.querySelector('#desktop-nav');
+  const links = document.querySelector('#desktop-nav ul').children;
+  for (let i = 0; i < links.length; i += 1) {
+    links[i].children[0].classList.toggle('text-white');
   }
-  nav.classList.toggle('mobile-nav')
+  nav.classList.toggle('mobile-nav');
 });
-
