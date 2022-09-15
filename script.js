da59a3ce45bbddd188ed89d376d4081e708c362a
Aimal Amiri <meaimal@hotmail.com>
2022-09-15 19:21:54 +0430

    The menu will close and the page will be moved to spicific place when the user clicks on the link

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const burger = document.querySelector('#mobile-menu');
+const nav = document.querySelector('#desktop-nav');
+const navLinks = document.querySelectorAll('.nav-link');
 
 burger.addEventListener('click', () => {
-  const nav = document.querySelector('#desktop-nav');
   const links = document.querySelector('#desktop-nav ul').children;
   for (let i = 0; i < links.length; i += 1) {
-    links[i].children[0].classList.toggle('text-white');
+    links[i].children[0].classList.add('text-white');
   }
   nav.classList.toggle('mobile-nav');
 });
+
+for (let i = 0; i < navLinks.length; i += 1) {
+  navLinks[i].addEventListener('click', () => {
+    nav.classList.remove('mobile-nav');
+  });
+}
